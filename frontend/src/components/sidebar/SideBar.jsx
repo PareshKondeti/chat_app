5350e1001b9ee196ef9af5393fc9417b54fb1a5a
PareshKondeti <ksaiparesh@gmail.com>
2025-01-05 21:01:32 +0530

    final2

--- frontend/src/components/sidebar/SideBar.jsx
+++ frontend/src/components/sidebar/SideBar.jsx
@@ -1,6 +1,6 @@
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
-import SearchInput from "./Searchinput";
+import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
 	return (
